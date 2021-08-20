983fe511411e5c7abfec37d2e0b720a4f332c470
Bianca <marcucci.bianca96@gmail.com>
2021-08-20 16:24:09 +0100

    committing an empty class to train using git and github

---
+++ character.js
@@ -0,0 +1,11 @@
+// parent of Wizard, Warrior
+// HP, Strenght, attacks monsters
+class Character{
+    constructor(char) {
+        this.char=char;
+    }
+    // attack monster
+    attack(){
+        
+    }
+}
